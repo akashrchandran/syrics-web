b781dc428db8a0042e1eb178abfdbfbc0fea6e15
Akash R Chandran <chandranrakash@gmail.com>
2023-02-03 11:43:57 +0530

    changed to flask webframework

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from spotify import get_album, get_track, get_play
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/spotify",  methods=['GET', 'POST'])
+def download():
+    if request.args:
+        if request.args.get('type') == 'album':
+            return render_template("spotify.html", data=get_album(request.args.get('id')))
+        elif request.args.get('type') == 'track':
+            return render_template("spotify.html", data=get_track(request.args.get('id')))
+        elif request.args.get('type') == 'playlist':
+            return render_template("spotify.html", data=get_play(request.args.get('id')))
+        else:
+            return "Invalid type"
+    elif request.form:
+        if request.form.get('type') == 'album':
+            return render_template("spotify.html", data=get_album(request.form.get('id')))
+        elif request.form.get('type') == 'track':
+            return render_template("spotify.html", data=get_track(request.form.get('id')))
+        elif request.form.get('type') == 'playlist':
+            return render_template("spotify.html", data=get_play(request.form.get('id')))
+        else:
+            return "Invalid type"
+    else:
+        return "No arguments provided"
+
+app.run(debug=True)
---
+++ spotify.py
@@ -0,0 +1,40 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cid = os.getenv("SPOTIFY_CLIENT_ID")
+secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_album(album_id):
+    album_data = sp.album(album_id)
+    album_data['artists'] = ','.join([artist['name'] for artist in album_data['artists']])
+    return {
+            "name": album_data['name'], 
+            "artist": album_data['artists'], 
+            "total_tracks": album_data['total_tracks'],
+            "release_date": album_data['release_date'],
+            "label": album_data['label'],
+            "image": album_data['images'][1]['url'],
+            "tracks": album_data['tracks']['items']
+            }
+
+def get_track(track_id):
+    return sp.track(track_id)
+
+def get_play(play_id):
+    play_data = sp.playlist(play_id)
+    play_data['owner'] = play_data['owner']['display_name']
+    play_data['total_tracks'] = play_data['tracks']['total']
+    play_data['collaborative'] = '[C]' if play_data['collaborative'] else ''
+    return {"name": play_data['name'], 
+            "owner": play_data['owner'], 
+            "total_tracks": play_data['total_tracks'], 
+            "collaborative": play_data['collaborative'],
+            "image": play_data['images'][1]['url'],
+            "tracks": play_data['tracks']['items']
+            }

363871b4d951fe8ceb69aeed365b0aec4a7057c3
Akash R Chandran <chandranrakash@gmail.com>
2023-03-13 23:29:13 +0530

    updated

--- index.py
+++ index.py
@@ -36,4 +36,4 @@ def get_all_tracks():
         return "No arguments provided", 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000)

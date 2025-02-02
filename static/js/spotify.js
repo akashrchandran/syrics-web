7a958af0b69385ea3bd5963ad9541572dc7d8109
Akash R Chandran <chandranrakash@gmail.com>
2025-02-02 21:20:21 +0530

    fix #33

--- static/js/spotify.js
+++ static/js/spotify.js
@@ -5,7 +5,7 @@ const lyricsSettings = JSON.parse(localStorage.getItem('lyricsSettings'));
 const lyricsType = lyricsSettings ? lyricsSettings.lyricsType : 'lrc';
 const fileNameFormat = lyricsSettings ? lyricsSettings.fileNameFormat : ["track_name", "track_no", "album"];
 
-async function get_lyrics(id, lyricsType) {
+async function get_lyrics(id) {
     const response = await fetch(`https://spotify-lyrics-api-pi.vercel.app/?trackid=${id}&format=${lyricsType}`);
     if (response.status != 200) {
         return [null, null];
@@ -125,7 +125,7 @@ maxDownload = async (type, id) => {
         let variable = parseInt((progress / length) * 100);
         bar.style.width = `${variable}%`;
         bar.textContent = `${variable}%`;
-        res_lyric = await get_lyrics(trackid, lyricsType);
+        res_lyric = await get_lyrics(trackid);
         const lyrics = res_lyric[0];
         if (lyrics != null) {
             if (lyricsType === 'lrc') {
@@ -174,11 +174,10 @@ downloadbtn.forEach((btn) => {
     btn.addEventListener('click', async () => {
         btn.innerHTML = '<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>';
         btn.classList.add('disabled');
-        const track_details = await fetch(`/api/tracks/${btn.getAttribute('data-id')}`).then(response => response.json());
         console.log(track_details);
         const attributes = ['data-id', 'data-name', 'data-album', 'data-artist', 'data-title', 'data-length'];
         const [id, name, album, artist, title, length] = attributes.map(attr => btn.getAttribute(attr));
-        const response = await get_lyrics(id, lyricsType); 
+        const response = await get_lyrics(id); 
         let lyrics = response[0];
         let sync = response[1];
         if (lyrics == null) {
